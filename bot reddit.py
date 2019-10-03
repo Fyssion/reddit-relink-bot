4c17199fdca26e58498b737d0806a3077eaebc95
computercrazy77 <32375789+computercrazy77@users.noreply.github.com>
2019-10-02 21:33:13 -0500

    finished!!

--- bot reddit.py
+++ bot reddit.py
@@ -16,12 +16,19 @@ reddit = praw.Reddit(client_id='fRymMJayGXty1g',
 client = discord.Client()
 
 #---Variables--
-TOKEN = "NjE4ODY4NjI2OTQ1OTk4ODQ5.XZUlCQ.k6FHbYk9Ur1JKGhoa8kVt-5IsOg"
+TOKEN = "NjE4ODY4NjI2OTQ1OTk4ODQ5.XZVFfw.cSS0Hf5xN6e2YbokYIwciZ359QA"
 reddit_ex = "r/"
 reddit_ex2 = "/r/"
-BOT_NAME = "ReLink"
+BOT_NAME = "Reddit ReLink"
 VERSION_NUMBER = "0.1.0"
-ICON = "https://media.discordapp.net/attachments/617425917106716689/617459186175049728/logo.png?width=457&height=457"
+ICON = "https://cdn.discordapp.com/attachments/402518339215294471/629138727322517507/user-interface-36-512.png"
+isnsfw = ""
+wosh = ""
+reddit_color = 0xFF4301
+#hyperlink_color = 0x0645AD
+hyperlink_color = 0x3366BB
+warning_color = 0xFFCC4D
+issub = False
 
 #--Embed--
 
@@ -29,43 +36,56 @@ ICON = "https://media.discordapp.net/attachments/617425917106716689/617459186175
 #--Commands--
 @client.event
 async def on_message(message):
+    global isnsfw
+    global wosh
+    global issub
     if reddit_ex in message.content:
         args = message.content.split("r/")
         afterslash = " ".join(args[1:])
         args = afterslash.split(" ")
         sub = " ".join(args[0:1])
-        l.info(str(message.author) + " tried to link r/" + sub + ".")
-        
+        l.info(str(message.author) + " tried to link to Subreddit " + sub + ".")
+
+
+        wosh = ""
+
+        if sub == "whosh" or sub == "wosh" or sub == "whoosh" or sub == "whooosh" or sub == "woosh" or sub == "wooosh"  or "oooo" in sub or "wosh" in sub or "whosh" in sub and sub != "woooosh":
+            wosh = "\nLooking for [r/woooosh](https://reddit.com/r/woooosh)?"
+
         subreddit_search = reddit.subreddits.search_by_name(sub, include_nsfw=True, exact=False)
         if sub in subreddit_search:
-            nsfw = "\n:warning:Subreddit is NSFW!:warning:"
+            isnsfw = "\n:warning:Subreddit is NSFW!:warning:"
+            issub = True
 
         subreddit_search2 = reddit.subreddits.search_by_name(sub, include_nsfw=False, exact=False)
         if sub in subreddit_search2:
-            nsfw = ""
+            isnsfw = ""
+            issub = True
 
         else:
             em_title = ":warning:Subreddit not found!"
-            em = discord.Embed(title = em_title, color=0xFF4301)
+            em_disc = "r/" + sub + " is not a subreddit." + isnsfw + wosh
+            em = discord.Embed(title = em_title, description = em_disc, color=warning_color)
             em.set_footer(text = str(BOT_NAME) + " • Version " + VERSION_NUMBER, icon_url = ICON)
             await message.channel.send(embed=em)
-            l.warning("Subreddt " + sub + " does not exist!")
-        
-        try:
+            l.warning("Subreddit '" + sub + "' does not exist!")
+            issub = False
+
+        if issub == True:
             subreddit = reddit.subreddit(sub)
 
-            em_sub_title = "[r/" + subreddit.display_name + "](https://reddit.com/r/" + subreddit.display_name + ")" + nsfw
+            em_sub_title = "[r/" + subreddit.display_name + "](https://reddit.com/r/" + subreddit.display_name + ")" + isnsfw + wosh
             em_title = subreddit.title
-            em = discord.Embed(title = em_title, description=em_sub_title, color=0xFF4301)
+            em_url = "https://reddit.com/r/" + subreddit.display_name
+            em = discord.Embed(title = em_title, description=em_sub_title, url = em_url, color=reddit_color)
+            em.set_thumbnail(url = subreddit.icon_img)
             em.set_footer(text = str(BOT_NAME) + " • Version " + VERSION_NUMBER, icon_url = ICON)
             await message.channel.send(embed=em)
-        except:
-            pass
 
 
     elif client.user.mentioned_in(message) and message.mention_everyone is False:
             l.info("Mentioned by " + str(message.author))
-            em_title = "Hey there, " + message.author.mention + "! I'm a bot that detects any reddit links and relinks them in clickable fashion!"
+            em_title = "Hey there, " + message.author.mention + "! I'm a bot that detects any Reddit links and relinks them in clickable fashion!"
             em = discord.Embed(description=em_title, color=0xFFFFFF)
             em.set_footer(text = str(BOT_NAME) + " • Version " + VERSION_NUMBER, icon_url = ICON)
             await message.channel.send(embed=em)
